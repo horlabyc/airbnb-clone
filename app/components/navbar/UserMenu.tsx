03b9204415d4c59a1a6d68d813c121aa080e6db5
Olaosebikan Sulaiman <olabisisulaiman@gmail.com>
2023-04-12 10:14:36 +0100

    Sign up WIP

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,11 +1,12 @@
 'use client'
+import useRegisterModal from '@/app/hooks/useRegisterModal'
 import { useCallback, useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar'
 import MenuItem from './MenuItem'
 const UserMenu = () => {
     const [isOpen, setIsOpen] = useState(false)
-    
+    const registerModal = useRegisterModal()
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value)
     }, [])
@@ -31,8 +32,8 @@ const UserMenu = () => {
             {isOpen && (
                 <div className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4 bg-white overflow-hidden right-0 top-12 text-sm">
                     <div className="flex flex-col cursor-pointer">
-                        <MenuItem onClick={() => {}} label="Login" />
-                        <MenuItem onClick={() => {}} label="Sign up"/>
+                        <MenuItem onClick={registerModal.onOpen} label="Login" />
+                        <MenuItem onClick={registerModal.onOpen} label="Sign up"/>
                     </div>
                 </div>
             )}
